490bff459531ad7641176dafac3218fdb663d57b
Canwaiting <455021735@qq.com>
2021-09-27 09:29:40 +0800

    1

--- experiment2/src/main/java/com/example/experiment2/ServletDemo.java
+++ experiment2/src/main/java/com/example/experiment2/ServletDemo.java
@@ -7,20 +7,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "just", value = "/ServletDemo")
+@WebServlet(name = "ServletDemo", value = "/ServletDemo")
 public class ServletDemo extends HttpServlet {
-    private String message;
+//    private String message;
 
-    public void init() { message = "we are in the ServletDemo"; }
+//    public void init() { message = "we are in the ServletDemo"; }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        String ID = request.getParameter("ID");
+        ID =ID.trim();
+        System.out.println(ID);
+        String information = "";
+        response.setContentType("text/html"); //todo
         PrintWriter out = response.getWriter();
+        if(ID=="001"){
+            information = "查询编号为001的用户信息如下:\n"
+                        + "用户名为:张三\n"
+                        + "用户年龄:20\n";
+        }
+        else{
+            information = "查询不到该用户的信息";
+        }
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + information + "</h1>");
         out.println("</body></html>");
+
     }
 
     public void destroy() {
